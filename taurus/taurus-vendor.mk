8ede583396a8e6121ef313195bdf2f356c85a0d9
huangqiwu <huangqiwu@xiaomi.com>
2014-01-28 14:44:07 +0800

    msm8960-common: initial
    N/A
    
     - copy from aries
    
    Change-Id: I9f222c579e740fdcb801cc1bdf9b1712cb4d99df
    Signed-off-by: huangqiwu <huangqiwu@xiaomi.com>

--- taurus/taurus-vendor.mk
+++ taurus/taurus-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013-2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,4 +18,5 @@
 DEVICE_PACKAGE_OVERLAYS := vendor/xiaomi/taurus/overlay
 
 $(call inherit-product, vendor/xiaomi/taurus/taurus-vendor-blobs.mk)
+$(call inherit-product, vendor/xiaomi/msm8960-common/msm8960-common-vendor.mk)
 $(call inherit-product, vendor/xiaomi/qcom-common/qcom-common-vendor.mk)
